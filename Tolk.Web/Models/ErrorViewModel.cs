5d547657ca0e44b171341825a4f9454882d6d106
Anders Abel <anders.abel@sustainsys.com>
2018-04-18 09:37:00 +0200

    File->New Project - MVC, Individual User Accounts (local, not cloud)

---
+++ Tolk.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tolk.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
