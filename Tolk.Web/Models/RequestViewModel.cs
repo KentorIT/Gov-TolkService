ba96f6b760165e48c05b1a91e063b5685923732c
Anders Abel <anders.abel@sustainsys.com>
2018-04-20 09:53:48 +0200

    form-entry Tag Helper

---
+++ Tolk.Web/Models/RequestViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tolk.Web.Models
+{
+    public class RequestViewModel
+    {
+        [Display(Name = "Plats")]
+        public string LocationName { get; set; }
+
+        [Display(Name = "Adress")]
+        public string LocationAddress { get; set; }
+
+        [Display(Name = "Ort")]
+        public string LocationCity { get; set; }
+    }
+}
---
+++ Tolk.Web/TagHelpers/FormEntryTagHelper.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Tolk.Web.TagHelpers
+{
+    public class FormEntryTagHelper: TagHelper
+    {
+        private readonly IHtmlGenerator htmlGenerator;
+        private readonly HtmlEncoder htmlEncoder;
+
+        public FormEntryTagHelper(IHtmlGenerator htmlGenerator, HtmlEncoder htmlEncoder)
+        {
+            this.htmlGenerator = htmlGenerator;
+            this.htmlEncoder = htmlEncoder;
+        }
+
+        private const string ForAttributeName = "asp-for";
+
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.Add("class", "form-group");
+
+            using (var writer = new StringWriter())
+            {
+                WriteLabel(writer);
+                WriteInput(writer);
+                output.Content.AppendHtml(writer.ToString());
+            }
+        }
+
+        private void WriteLabel(TextWriter writer)
+        {
+            var tagBuilder = htmlGenerator.GenerateLabel(
+                ViewContext,
+                For.ModelExplorer,
+                For.Name,
+                labelText: null,
+                htmlAttributes: new { @class = "control-label" });
+
+            tagBuilder.WriteTo(writer, htmlEncoder);
+        }
+
+        private void WriteInput(TextWriter writer)
+        {
+            var tagBuilder = htmlGenerator.GenerateTextBox(
+                ViewContext,
+                For.ModelExplorer,
+                For.Name,
+                value: null,
+                format: null,
+                htmlAttributes: new { @class = "form-control" });
+
+            tagBuilder.WriteTo(writer, htmlEncoder);
+        }
+    }
+}
