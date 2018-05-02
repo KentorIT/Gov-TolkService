42dc87431a93816d23d242e7079d5cdd811c399a
Anders Abel <anders.abel@sustainsys.com>
2018-05-02 14:21:49 +0200

    Add DB migration to get in sync - The ASP.NET Identity migration in the template obviously isn't generating   the same migration metadata as the current tooling.

--- Tolk.BusinessLogic/Data/Migrations/20180502112258_AspNetIdentityGetInSync.Designer.cs
+++ Tolk.BusinessLogic/Data/Migrations/20180502112258_AspNetIdentityGetInSync.Designer.cs
@@ -11,9 +11,10 @@ using Tolk.BusinessLogic.Data;
 namespace Tolk.BusinessLogic.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180502112258_AspNetIdentityGetInSync")]
+    partial class AspNetIdentityGetInSync
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
