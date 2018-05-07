0c79d1fd1b7f07ece3353e6972622ed9a58ee5b2
Anders Abel <anders.abel@sustainsys.com>
2018-05-07 08:26:34 +0200

    Add data migration for Region - Update EF Core to 2.1.0 preview to get access to data migrations

--- Tolk.BusinessLogic/Data/TolkDbContext.cs
+++ Tolk.BusinessLogic/Data/TolkDbContext.cs
@@ -17,6 +17,9 @@ namespace Tolk.BusinessLogic.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<Region>()
+                .HasData(Region.SeedData);
         }
 
         public DbSet<Region> Regions { get; set; }
--- Tolk.BusinessLogic/Entities/Region.cs
+++ Tolk.BusinessLogic/Entities/Region.cs
@@ -11,5 +11,30 @@ namespace Tolk.BusinessLogic.Entities
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public static Region[] SeedData { get; } = new[]
+        {
+                new Region { Id = 1, Name = "Stockholm" },
+                new Region { Id = 2, Name = "Uppsala" },
+                new Region { Id = 3, Name = "Södermanland" },
+                new Region { Id = 4, Name = "Östergötland" },
+                new Region { Id = 5, Name = "Jönköping" },
+                new Region { Id = 6, Name = "Kronoberg" },
+                new Region { Id = 7, Name = "Kalmar" },
+                new Region { Id = 80, Name = "Gotland" },
+                new Region { Id = 8, Name = "Blekinge " },
+                new Region { Id = 25, Name = "Skåne" },
+                new Region { Id = 11, Name = "Halland" },
+                new Region { Id = 13, Name = "Västra Götaland" },
+                new Region { Id = 15, Name = "Värmland" },
+                new Region { Id = 16, Name = "Örebro" },
+                new Region { Id = 17, Name = "Västmanland" },
+                new Region { Id = 18, Name = "Dalarna" },
+                new Region { Id = 19, Name = "Gävleborg" },
+                new Region { Id = 20, Name = "Västernorrland" },
+                new Region { Id = 21, Name = "Jämtland Härjedalen" },
+                new Region { Id = 22, Name = "Västerbotten" },
+                new Region { Id = 23, Name = "Norrbotten" }
+        };
     }
 }
