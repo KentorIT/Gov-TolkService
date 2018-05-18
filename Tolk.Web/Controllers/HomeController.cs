83b93ef117ffd95e099756b5a46d3f450f8399c9
johansvard <johan.svard@kentor.se>
2018-05-18 08:35:42 +0200

    #KT-131: Skapa lista på förmedlings förstasida som visar inkomna förfrågningar
    - Added counters to start pages

--- Tolk.Web/Controllers/HomeController.cs
+++ Tolk.Web/Controllers/HomeController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Tolk.BusinessLogic.Data;
+using Tolk.BusinessLogic.Entities;
+using Tolk.BusinessLogic.Enums;
 using Tolk.Web.Models;
 
 namespace Tolk.Web.Controllers
@@ -12,10 +15,12 @@ namespace Tolk.Web.Controllers
     public class HomeController : Controller
     {
         private TolkDbContext _dbContext;
+        private readonly UserManager<AspNetUser> _userManager;
 
-        public HomeController(TolkDbContext dbContext)
+        public HomeController(TolkDbContext dbContext, UserManager<AspNetUser> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
 
         public IActionResult Index(string message)
@@ -24,8 +29,68 @@ namespace Tolk.Web.Controllers
             {
                 return RedirectToAction("CreateInitialUser", "Account");
             }
+            var startBoxes = GetStartPageBoxes();
+            return View(new StartViewModel { Message = message, Boxes = startBoxes });
+        }
 
-            return View(new StartViewModel { Message = message });
+        private IEnumerable<StartPageBox> GetStartPageBoxes()
+        {
+            var customerId = User.Claims.SingleOrDefault(c => c.Type == TolkClaimTypes.CustomerOrganisationId)?.Value;
+            if (customerId != null)
+            {
+                yield return new StartPageBox
+                {
+                    Count = _dbContext.Orders.Where(o => o.Status == OrderStatus.RequestResponded && o.CreatedBy == _userManager.GetUserId(User)).Count(),
+                    Header = "Tillsatt tolk",
+                    Controller = "Order",
+                    Action = "List"
+                };
+                yield return new StartPageBox
+                {
+                    Count = _dbContext.Orders.Where(o => o.Status == OrderStatus.Delivered && o.CreatedBy == _userManager.GetUserId(User)).Count(),
+                    Header = "Rekvirerade",
+                    Controller = "Order",
+                    Action = "List"
+                };
+            }
+            var brokerId = User.Claims.SingleOrDefault(c => c.Type == TolkClaimTypes.BrokerId)?.Value;
+            if (brokerId != null)
+            {
+                yield return new StartPageBox
+                {
+                    Count = _dbContext.Requests.Where(r => (r.Status == RequestStatus.Created || r.Status == RequestStatus.Received) && 
+                        r.Ranking.BrokerId == int.Parse(brokerId)).Count(),
+                    Header = "Inkomna förfrågningar",
+                    Controller = "Request",
+                    Action = "List"
+                };
+                yield return new StartPageBox
+                {
+                    Count = 0,
+                    Header = "Ändrade förfrågningar",
+                    Controller = "Request",
+                    Action = "List"
+                };
+            }
+            var interpreterId = User.Claims.SingleOrDefault(c => c.Type == TolkClaimTypes.InterpreterId)?.Value;
+            if (interpreterId != null)
+            {
+                yield return new StartPageBox
+                {
+                    //TODO: Here we need to check the order too!
+                    Count = _dbContext.Requests.Where(r => (r.Status == RequestStatus.Approved) && r.InterpreterId == int.Parse(interpreterId)).Count(),
+                    Header = "Tillsatta uppdrag",
+                    Controller = "Assignment",
+                    Action = "List"
+                };
+                yield return new StartPageBox
+                {
+                    Count = 0,
+                    Header = "Att avrapportera",
+                    Controller = "Assignment",
+                    Action = "List"
+                };
+            }
         }
 
         public IActionResult About()
--- Tolk.Web/Models/StartViewModel.cs
+++ Tolk.Web/Models/StartViewModel.cs
@@ -9,5 +9,15 @@ namespace Tolk.Web.Models
     public class StartViewModel
     {
         public string Message { get; set; }
+        public IEnumerable<StartPageBox> Boxes { get; set; }
+    }
+
+    public class StartPageBox
+    {
+        public string Header { get; set; }
+        public int Count { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public IDictionary<string, string> RouteData { get; set; }
     }
 }
