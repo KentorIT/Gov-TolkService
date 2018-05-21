d50d28e24c571794abc52b5e9499dc1da5d994bc
johansvard <johan.svard@kentor.se>
2018-05-21 10:22:34 +0200

    Fixed some with the reset script

--- Tolk.BusinessLogic/BaseData/ResetData.sql
+++ Tolk.BusinessLogic/BaseData/ResetData.sql
@@ -1,17 +1,18 @@
+Use TolkDev
 declare @increment bit
 declare @reseed int
 
---TODO: Clear the users to...
-truncate table InterpreterBrokerRegion
 truncate table OrderRequirements
+truncate table Requests
 
-select @increment = Count(*)
+select @increment = IsNull(Max(OrderId), 1)
 from Orders 
 set @reseed = 1 - @increment
 
+Select @reseed
+
 delete Orders --
 DBCC CHECKIDENT (Orders, reseed, @reseed)--
+/**/
 
--- Remove roles that are no longer directly assigned (instead policies are used
--- that relies on present of CustomerId etc.
-delete AspNetRoles where Id in ('TolkBrokerRole', 'TolkCustomerRole', 'TolkInterpreterRole')
+Select * from Orders
