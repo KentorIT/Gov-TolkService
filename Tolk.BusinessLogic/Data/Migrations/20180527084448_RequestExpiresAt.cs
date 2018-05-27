f8f6b3b034d45ecd830951d434d0dc3c27abd11e
Anders Abel <anders.abel@sustainsys.com>
2018-05-27 10:46:31 +0200

    Migration for Request.ExpiresAt - Was missing, should have been in previous commit.

---
+++ Tolk.BusinessLogic/Data/Migrations/20180527084448_RequestExpiresAt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tolk.BusinessLogic.Data.Migrations
+{
+    public partial class RequestExpiresAt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExpiresAt",
+                table: "Requests",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiresAt",
+                table: "Requests");
+        }
+    }
+}
