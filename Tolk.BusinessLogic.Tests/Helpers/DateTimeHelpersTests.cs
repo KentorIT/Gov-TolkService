f702e54581c45bc64ef788a8d6961a5b21d4f723
Anders Abel <anders.abel@sustainsys.com>
2018-05-28 10:53:36 +0200

    #KT-38 Avropsmotor - Calculation of workdays

---
+++ Tolk.BusinessLogic.Tests/Helpers/DateTimeHelpersTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tolk.BusinessLogic.Helpers;
+using Xunit;
+using FluentAssertions;
+
+namespace Tolk.BusinessLogic.Tests.Helpers
+{
+    public class DateTimeHelpersTests
+    {
+        [Fact]
+        public void GetWorkDaysBetween_ThrowsIfFirstDateIsAfterSecondDate()
+        {
+            Action a = () => DateTimeHelpers.GetWorkDaysBetween(new DateTime(1), new DateTime(0));
+
+            a.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void GetWorkDaysBetween_ThrowsIfFirstDateIsNotPlainDate()
+        {
+            Action a = () => DateTimeHelpers.GetWorkDaysBetween(new DateTime(2018, 06, 04, 14, 00, 00), new DateTime(2018, 06, 05));
+
+            a.Should().Throw<ArgumentException>()
+                .And.ParamName.Should().Be("firstDate");
+        }
+
+        [Fact]
+        public void GetWorkDaysBetween_ThrowsIfSecondDateIsNotPlainDate()
+        {
+            Action a = () => DateTimeHelpers.GetWorkDaysBetween(new DateTime(2018, 06, 04), new DateTime(2018, 06, 05, 14, 00, 00));
+
+            a.Should().Throw<ArgumentException>()
+                .And.ParamName.Should().Be("secondDate");
+        }
+
+        [Fact]
+        public void GetWorkDaysBetween_ThrowsDateTimeKindsAreDifferent()
+        {
+            Action a = () => DateTimeHelpers.GetWorkDaysBetween(
+                new DateTime(2018, 5, 1, 0, 0, 0, DateTimeKind.Local),
+                new DateTime(2018, 6, 1, 0, 0, 0, DateTimeKind.Unspecified));
+
+            a.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        // Note 2016-08-01 was a Monday.
+        [InlineData("2016-08-01", "2016-08-04", 3)] // Monday-Thursday
+        [InlineData("2016-08-01", "2016-08-08", 5)] // Monday-Monday
+        [InlineData("2016-08-04", "2016-08-08", 2)] // Thursday-Monday
+        [InlineData("2016-08-06", "2016-08-09", 1)] // Saturday-Tuesday
+        [InlineData("2016-08-07", "2016-08-09", 1)] // Sunday-Tuesday
+        [InlineData("2016-08-01", "2016-08-06", 4)] // Monday-Saturday
+        [InlineData("2016-08-06", "2016-08-07", 0)] // Saturday-Sunday
+        [InlineData("2016-08-06", "2016-08-14", 5)] // Saturday-Sunday
+        public void GetWorkDaysBetween(string firstDate, string secondDate, int actual)
+        {
+            DateTimeHelpers.GetWorkDaysBetween(DateTime.Parse(firstDate), DateTime.Parse(secondDate))
+                .Should().Be(actual, "there are {0} workdays between {1} and {2}", actual, firstDate, secondDate);
+        }
+    }
+}
---
+++ Tolk.BusinessLogic/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tolk.BusinessLogic.Helpers
+{
+    public static class DateTimeHelpers
+    {
+        public static int GetWorkDaysBetween(DateTime firstDate, DateTime secondDate)
+        {
+            // https://stackoverflow.com/questions/2510383/how-can-i-calculate-what-date-good-friday-falls-on-given-a-year
+            if (secondDate < firstDate)
+            {
+                throw new ArgumentException("First Date must be before secondDate");
+            }
+
+            if (firstDate.TimeOfDay.Ticks != 0)
+            {
+                throw new ArgumentException($"{nameof(firstDate)} includes a time other than midnight. Use the Date property to get a pure date.", nameof(firstDate));
+            }
+
+            if (secondDate.TimeOfDay.Ticks != 0)
+            {
+                throw new ArgumentException($"{nameof(secondDate)} includes a time other than midnight. Use the Date property to get a pure date.", nameof(secondDate));
+            }
+
+            if(firstDate.Kind != secondDate.Kind)
+            {
+                throw new ArgumentException($"{nameof(firstDate)} has kind {firstDate.Kind} which is different from {nameof(secondDate)} kind {secondDate.Kind}");
+            }
+
+            int fullWeeks = ((secondDate - firstDate).Days / 7);
+
+            int rest = ((secondDate - firstDate).Days % 7);
+
+            if(secondDate.DayOfWeek < firstDate.DayOfWeek) // Wraps over a weekend.
+            {
+                rest = rest == 1 ? 0 : rest - 2; // Special case if start is Saturday and end is Sunday.
+            }
+            else
+            {
+                if(firstDate.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    rest -= 1;
+                }
+
+                if (secondDate.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    rest -= 1;
+                }
+            }
+
+            return fullWeeks * 5 + rest;
+        }
+    }
+}
