a86eaa603df69653aacff960c6fa83459f8c0a2d
Anders Abel <anders.abel@sustainsys.com>
2018-05-28 12:37:50 +0200

    Make Holidays.Date a plain date (without time part)

---
+++ Tolk.BusinessLogic/Data/Migrations/20180528102656_HolidayDateAsPlainDate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tolk.BusinessLogic.Data.Migrations
+{
+    public partial class HolidayDateAsPlainDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey("PK_Holidays", "Holidays");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Holidays",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AddPrimaryKey("PK_Holidays", "Holidays", "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey("PK_Holidays", "Holidays");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Holidays",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AddPrimaryKey("PK_Holidays", "Holidays", "Date");
+        }
+    }
+}
