f458e9dfc5d8b77f58b35804d25d1ce720b2cc57
Anders Abel <anders.abel@sustainsys.com>
2018-06-13 10:38:17 +0200

    Remove interpreter from dev data - It will be possible to create through registration and the current code   didn't work after change to InterpreterBroker

--- Tolk.BusinessLogic/BaseData/dev.Users.sql
+++ Tolk.BusinessLogic/BaseData/dev.Users.sql
@@ -2,9 +2,6 @@
 
 DELETE AspNetUsers WHERE Email in ('pia@polisen.se', 'david@domstol.se', 'frida@formedling.se', 'tomas@tolk.se')
 
-if not exists (select 1 from Interpreters)
-INSERT Interpreters DEFAULT VALUES
-
 INSERT AspNetUsers
 (ConcurrencyStamp, Email, NormalizedEmail, NormalizedUserName, SecurityStamp, UserName, AccessFailedCount, EmailConfirmed, LockoutEnabled, PhoneNumberConfirmed, TwoFactorEnabled, CustomerOrganisationId, BrokerId, InterpreterId)
 VALUES
@@ -15,9 +12,4 @@ VALUES
 ('2735d701-afe5-4d12-8e18-e87d10e793fe', 'frida@formedling.se', 'FRIDA@FORMEDLING.SE', 'FRIDA@FORMEDLING.SE', 'ca04c2bd-f6f5-4f1d-a4e6-80ab6fd5c34b', 'frida@formedling.se', 0, 1, 1, 0, 0, null, 1, null),
 ('2735d701-afe5-4d12-8e18-e87d10e793ff', 'fredrik@formedling2.se', 'FREDRIK@FORMEDLING2.SE', 'FREDRIK@FORMEDLING2.SE', 'ca04c2bd-f6f5-4f1d-a4e6-80ab6fd5c34c', 'fredrik@formedling2.se', 0, 1, 1, 0, 0, null, 2, null),
 ('2735d701-afe5-4d12-8e18-e87d10e79400', 'fabian@formedling3.se', 'FABIAN@FORMEDLING3.SE', 'FABIAN@FORMEDLING3.SE', 'ca04c2bd-f6f5-4f1d-a4e6-80ab6fd5c34d', 'fabian@formedling3.se', 0, 1, 1, 0, 0, null, 3, null),
-('2735d701-afe5-4d12-8e18-e87d10e79401', 'filippa@formedling4.se', 'FILIPPA@FORMEDLING4.SE', 'FILIPPA@FORMEDLING4.SE', 'ca04c2bd-f6f5-4f1d-a4e6-80ab6fd5c34e', 'filippa@formedling4.se', 0, 1, 1, 0, 0, null, 4, null),
-('c7293f8c-caea-4262-bc4c-d869ff5a5644', 'tomas@tolk.se', 'TOMAS@TOLK.SE', 'TOMAS@TOLK.SE', '163ce957-af1b-48a6-8dd1-d07c45d308e9', 'tomas@tolk.se', 0, 1, 1, 0, 0, null, null, 1)
-
-Insert InterpreterBrokerRegion
-(RegionId, BrokerId, InterpreterId)
-Select RegionId, BrokerId, 1 from BrokerRegions
+('2735d701-afe5-4d12-8e18-e87d10e79401', 'filippa@formedling4.se', 'FILIPPA@FORMEDLING4.SE', 'FILIPPA@FORMEDLING4.SE', 'ca04c2bd-f6f5-4f1d-a4e6-80ab6fd5c34e', 'filippa@formedling4.se', 0, 1, 1, 0, 0, null, 4, null)
