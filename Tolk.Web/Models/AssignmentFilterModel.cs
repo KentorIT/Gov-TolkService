1a237db9e18335857e510bdf77854bc7c03d995e
WINELL Liv <liv.winell@soprasteria.com>
2018-08-06 12:08:59 +0200

    #KT-193 - Klick på "Kommande uppdrag" och "Att avrapportera" i tolkens vy leder till fel sida: Added filter for status (fictitious) of assignment and a new model for listing assignments. Corrected misspelling.

---
+++ Tolk.Web/Models/AssignmentFilterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Tolk.BusinessLogic.Enums;
+
+
+namespace Tolk.Web.Models
+{
+    public class AssignmentFilterModel
+    {
+        [Display(Name = "Uppdragets status")]
+        public AssignmentStatus? Status { get; set; }
+    }
+}
---
+++ Tolk.Web/Models/AssignmentListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tolk.Web.Models
+{
+    public class AssignmentListModel
+    {
+        public AssignmentFilterModel FilterModel { get; set; }
+
+        public IEnumerable<RequestListItemModel> Items { get; set; }
+    }
+}
