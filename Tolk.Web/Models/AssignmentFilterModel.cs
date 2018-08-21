1edcc0a49e65261d4bac8f2107cdf8fac63f1c70
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2018-08-21 13:50:09 +0200

    #KT-285: Som tolk kunna söka med filter på avrop
    - Added new filtering logic in AssignmentController.List
    - Added new filtering properties in AssignmentFilterModel
    - Moved heading to top in Views/Assignment/List, consistency
    - Added new filtering options in Views/Assignment/_Filter

--- Tolk.Web/Models/AssignmentFilterModel.cs
+++ Tolk.Web/Models/AssignmentFilterModel.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Tolk.BusinessLogic.Enums;
-
+using Tolk.BusinessLogic.Utilities;
 
 namespace Tolk.Web.Models
 {
     public class AssignmentFilterModel
     {
+        [Display(Name = "Avrops-ID")]
+        public string OrderNumber { get; set; }
+
+        [Display(Name = "Region")]
+        public int? RegionId { get; set; }
+
+        [Display(Name = "Kund")]
+        public int? CustomerOrganizationId { get; set; }
+
+        [Display(Name = "Språk")]
+        public int? LanguageId { get; set; }
+
+        [Display(Name = "Startdatum")]
+        public DateRange StartDateRange { get; set; }
+
         [Display(Name = "Uppdragets status")]
         public AssignmentStatus? Status { get; set; }
     }
