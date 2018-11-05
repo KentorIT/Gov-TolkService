e2045379ee87565312cb40727e79a50d2eb17d6e
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2018-11-05 10:56:25 +0100

    #KT-475: UX - Undersök less - Added LESS support

---
+++ Tolk.Web/gulpfile.js
@@ -0,0 +1,9 @@
+﻿/// <binding BeforeBuild='less' />
+var gulp = require("gulp");
+var less = require("gulp-less");
+
+gulp.task("less", function () {
+    return gulp.src("wwwroot/css/site.less")
+        .pipe(less())
+        .pipe(gulp.dest("wwwroot/css"));
+});
