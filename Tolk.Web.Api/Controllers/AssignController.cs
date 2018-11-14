c59a430b583df398940d5d0c87c108b7a86b60ec
johansvard <johan.svard@kentor.se>
2018-11-14 12:30:24 +0100

    Added Api project

---
+++ Tolk.Web.Api/Controllers/AssignController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tolk.Api.Payloads;
+
+namespace Tolk.Web.Api.Controllers
+{
+    [Route("api/Request/[controller]")]
+    [ApiController]
+
+    public class AssignController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<bool> Post([FromBody] RequestAssignModel model)
+        {
+            X509Certificate2 clientCertInRequest = Request.HttpContext.Connection.ClientCertificate;
+            //if (!clientCertInRequest.Verify() || !AllowedCerialNumbers(clientCertInRequest.SerialNumber))
+            //{
+            //    Response.StatusCode = 404;
+            //    return null;
+            //}
+            return true;
+        }
+    }
+}
---
+++ Tolk.Web.Api/Controllers/RequestsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tolk.Api.Payloads;
+
+namespace Tolk.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestsController : ControllerBase
+    {
+        // GET api/requests
+        [HttpGet]
+        public ActionResult<IEnumerable<RequestModel>> Get()
+        {
+            return new RequestModel[] {
+                new RequestModel { OrderNumber = "xx" },
+                new RequestModel { OrderNumber = "yy" },
+            };
+        }
+
+        // GET api/requests/xx
+        [HttpGet("{orderNumber}")]
+        public ActionResult<RequestModel> Get(string orderNumber)
+        {
+            return new RequestModel { OrderNumber = orderNumber};
+        }
+
+        [HttpGet]
+        public ActionResult<bool> Ping()
+        {
+            X509Certificate2 clientCertInRequest = Request.HttpContext.Connection.ClientCertificate;
+            //if (!clientCertInRequest.Verify() || !AllowedCerialNumbers(clientCertInRequest.SerialNumber))
+            //{
+            //    Response.StatusCode = 404;
+            //    return null;
+            //}
+            return true;
+        }
+
+        public ActionResult<bool> Assign([FromBody] RequestAssignModel model)
+        {
+            X509Certificate2 clientCertInRequest = Request.HttpContext.Connection.ClientCertificate;
+            //if (!clientCertInRequest.Verify() || !AllowedCerialNumbers(clientCertInRequest.SerialNumber))
+            //{
+            //    Response.StatusCode = 404;
+            //    return null;
+            //}
+            return true;
+        }
+
+    }
+
+}
