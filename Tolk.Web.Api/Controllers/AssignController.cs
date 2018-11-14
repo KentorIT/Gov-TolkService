57559cf5fc90fc7af9c57b6791fdf8c28b9ffdee
johansvard <johan.svard@kentor.se>
2018-11-14 12:41:28 +0100

    Now all is in, for test purposes

--- Tolk.Web.Api/Controllers/AssignController.cs
+++ Tolk.Web.Api/Controllers/AssignController.cs
@@ -5,23 +5,32 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tolk.Api.Payloads;
+using Tolk.BusinessLogic.Data;
 
 namespace Tolk.Web.Api.Controllers
 {
     [Route("api/Request/[controller]")]
     [ApiController]
-
     public class AssignController : ControllerBase
     {
+        private readonly TolkDbContext _dbContext;
+
+        public AssignController(TolkDbContext tolkDbContext)
+        {
+            _dbContext = tolkDbContext;
+        }
+
         [HttpPost]
         public ActionResult<bool> Post([FromBody] RequestAssignModel model)
         {
             X509Certificate2 clientCertInRequest = Request.HttpContext.Connection.ClientCertificate;
-            //if (!clientCertInRequest.Verify() || !AllowedCerialNumbers(clientCertInRequest.SerialNumber))
-            //{
-            //    Response.StatusCode = 404;
-            //    return null;
-            //}
+            var order = _dbContext.Orders.SingleOrDefault(o => o.OrderNumber == model.OrderNumber);
+            if (order == null)
+            //if (!clientCertInRequest.Verify() || !AllowedSerialNumbers(clientCertInRequest.SerialNumber))
+            {
+                Response.StatusCode = 401;
+                return false;
+            }
             return true;
         }
     }
