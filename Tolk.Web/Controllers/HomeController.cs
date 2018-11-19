41d543d824fd79860426dec9f069c2d92c87e1ea
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2018-11-19 13:37:39 +0100

    #KT-541: UX - Lägg till URL:er och logotyp i footern - Replaced placeholder links in the footer with actual links (now just missing contact information to support - Created actual placeholder FAQ page

--- Tolk.Web/Controllers/HomeController.cs
+++ Tolk.Web/Controllers/HomeController.cs
@@ -344,6 +344,11 @@ namespace Tolk.Web.Controllers
             return View();
         }
 
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
