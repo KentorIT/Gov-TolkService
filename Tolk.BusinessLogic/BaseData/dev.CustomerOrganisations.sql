43793b73a1d87b7d52173830c23109ca485555fa
WINELL Liv <liv.winell@soprasteria.com>
2018-12-10 11:16:43 +0100

    #KT-589 - Uppdatera myndighetsdomänerna, ny lista: Updated in test environments.

--- Tolk.BusinessLogic/BaseData/dev.CustomerOrganisations.sql
+++ Tolk.BusinessLogic/BaseData/dev.CustomerOrganisations.sql
@@ -3,9 +3,9 @@ use TolkDev
 set identity_insert CustomerOrganisations on 
 
 insert CustomerOrganisations(CustomerOrganisationId, Name, PriceListType, EmailDomain)
-Values(1, 'Polisen', 2, 'polisen.se'),
+Values(1, 'Polismyndigheten', 2, 'polisen.se'),
 (2, 'Migrationsverket', 2, 'migrationsverket.se'),
-(3, 'Domstolsverket', 1, 'domstol.se'),
+(3, 'Domstolsverket', 1, 'dom.se'),
 (4, 'Sopra Steria', 2, 'soprasteria.com')
 
 
