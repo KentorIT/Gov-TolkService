ff14b2b647a2130567dbd78d8355c1c7f81de482
johansvard <johan.svard@kentor.se>
2018-12-18 14:38:49 +0100

    Added official interpreter id to contact info on an interpreter.

--- Tolk.BusinessLogic/Entities/InterpreterBroker.cs
+++ Tolk.BusinessLogic/Entities/InterpreterBroker.cs
@@ -34,7 +34,7 @@ namespace Tolk.BusinessLogic.Entities
 
         public string FullName { get => $"{FirstName} {LastName}"; }
 
-        public string CompleteContactInformation { get => $"{FullName}\nE-post: {Email ?? "-"}\nTel: {PhoneNumber ?? "-"}"; }
+        public string CompleteContactInformation { get => $"{FullName}\nTolk-ID: {OfficialInterpreterId ?? "-"}\nE-post: {Email ?? "-"}\nTel: {PhoneNumber ?? "-"}"; }
 
         public string OfficialInterpreterId { get; set; }
 
