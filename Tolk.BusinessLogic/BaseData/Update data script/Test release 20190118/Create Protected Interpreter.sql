bad3142a0699ceb55374c434850e08d76ec5008f
henrikmaurin <henrik.maurin@soprasteria.com>
2019-01-18 14:48:20 +0100

    #KT-696 Interpreter with protected identity

---
+++ Tolk.BusinessLogic/BaseData/Update data script/Test release 20190118/Create Protected Interpreter.sql
@@ -0,0 +1,7 @@
+﻿  BEGIN TRAN
+  
+  INSERT INTO [dbo].[InterpreterBrokers]
+  (BrokerId, Email, FirstName, LastName, OfficialInterpreterId)  
+  SELECT BrokerId, 'tolk@'+EmailDomain, 'Tolk', 'Skyddad Identitet', 1 FROM [dbo].[Brokers];
+
+  COMMIT TRAN;
