f938f6a3dd1fc7e45f25059b219fd72efe090797
henrikmaurin <henrik.maurin@soprasteria.com>
2019-01-21 16:36:04 +0100

    #KT-675 Remove clock symbol

--- Tolk.Web/TagHelpers/FormEntryTagHelper.cs
+++ Tolk.Web/TagHelpers/FormEntryTagHelper.cs
@@ -519,15 +519,16 @@ namespace Tolk.Web.TagHelpers
 
             RemoveRequiredIfNullable(tagBuilder);
             tagBuilder.WriteTo(writer, _htmlEncoder);
-
-            writer.WriteLine("<div class=\"input-group-addon\"><span class=\"glyphicon glyphicon-time\"></span></div></div>"); //input-group time
+            writer.WriteLine("</div>");
+            //writer.WriteLine("<div class=\"input-group-addon\"><span class=\"glyphicon glyphicon-time\"></span></div></div>"); //input-group time
         }
 
         private void WriteSplitTimePickerInput(ModelExplorer timeModelExplorer, string timeFieldName, object timeValue, TextWriter writer, bool hour)
         {
             writer.WriteLine("<div class=\"input-group time timesplit\">");
             WriteSelect(GetSplitTImeValues(hour), writer, timeFieldName, timeModelExplorer, hour ? "tim" : "min");
-            writer.WriteLine("<div class=\"input-group-addon\"><span class=\"glyphicon glyphicon-time\"></span></div></div>"); //input-group time
+            writer.WriteLine("</div>");
+            //writer.WriteLine("<div class=\"input-group-addon\"><span class=\"glyphicon glyphicon-time\"></span></div></div>"); //input-group time
             WriteValidation(writer, timeModelExplorer, timeFieldName);
         }
 
