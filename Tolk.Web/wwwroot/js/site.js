37796f79bb2de30055a0342e0634dd70c5ceaad7
henrikmaurin <henrik.maurin@soprasteria.com>
2019-01-22 10:29:46 +0100

    #KT-528 Sort by createddate descending

--- Tolk.Web/wwwroot/js/site.js
+++ Tolk.Web/wwwroot/js/site.js
@@ -139,6 +139,7 @@ $(function () {
 
     $(".table-paging table").DataTable({
         searching: false,
+        order: [[0, 'desc']],
         language: {
             url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/Swedish.json"
         }
