26b25d60ad7e9ca5a7694195feaf5ba3cdc4d51a
johansvard <johan.svard@kentor.se>
2019-02-07 12:53:01 +0100

    Minor refactoring for the rewrite fix...

---
+++ Tolk.Web/Authorization/RedirectHostRule.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Tolk.Web.Authorization
+{
+    public class RedirectHostRule : IRule
+    {
+        public int StatusCode { get; } = (int)HttpStatusCode.MovedPermanently;
+        public bool ExcludeLocalhost { get; set; } = true;
+        public string InternalHost { get; set; }
+        public string OfficialSiteUrl { get; set; }
+
+        public void ApplyRule(RewriteContext context)
+        {
+            var request = context.HttpContext.Request;
+            var host = request.Host;
+            if (ExcludeLocalhost && string.Equals(host.Host, "localhost", StringComparison.OrdinalIgnoreCase))
+            {
+                context.Result = RuleResult.ContinueRules;
+                return;
+            }
+
+            if (string.Equals(host.Host, InternalHost, StringComparison.OrdinalIgnoreCase))
+            {
+                var response = context.HttpContext.Response;
+                response.StatusCode = StatusCode;
+                response.Headers[HeaderNames.Location] =
+                    $"{OfficialSiteUrl}{request.PathBase}{request.Path}{request.QueryString}";
+                context.Result = RuleResult.EndResponse; // Do not continue processing the request     
+                return;
+            }
+            context.Result = RuleResult.ContinueRules;
+            return;
+        }
+    }
+}
--- Tolk.Web/Startup.cs
+++ Tolk.Web/Startup.cs
@@ -8,10 +8,8 @@ using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Net.Http.Headers;
 using System;
 using System.Globalization;
-using System.Net;
 using Tolk.BusinessLogic.Data;
 using Tolk.BusinessLogic.Entities;
 using Tolk.BusinessLogic.Helpers;
@@ -184,36 +182,6 @@ namespace Tolk.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-        }
-    }
-    public class RedirectHostRule : IRule
-    {
-        public int StatusCode { get; } = (int)HttpStatusCode.MovedPermanently;
-        public bool ExcludeLocalhost { get; set; } = true;
-        public string InternalHost { get; set; }
-        public string OfficialSiteUrl { get; set; }
-
-        public void ApplyRule(RewriteContext context)
-        {
-            var request = context.HttpContext.Request;
-            var host = request.Host;
-            if (ExcludeLocalhost && string.Equals(host.Host, "localhost", StringComparison.OrdinalIgnoreCase))
-            {
-                context.Result = RuleResult.ContinueRules;
-                return;
-            }
-
-            if (string.Equals(host.Host, InternalHost, StringComparison.OrdinalIgnoreCase))
-            {
-                var response = context.HttpContext.Response;
-                response.StatusCode = StatusCode;
-                response.Headers[HeaderNames.Location] =
-                    $"{OfficialSiteUrl}{request.PathBase}{request.Path}{request.QueryString}";
-                context.Result = RuleResult.EndResponse; // Do not continue processing the request     
-                return;
-            }
-            context.Result = RuleResult.ContinueRules;
-            return;
         }
     }
 }
