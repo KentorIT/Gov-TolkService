ced15659f6642a2a996dd61a35930b11914f3a3b
WINELL Liv <liv.winell@soprasteria.com>
2019-04-14 17:08:21 +0200

    #KT-858 - Inaktivera en tolk: Fix with dev. data script.

--- Tolk.BusinessLogic/BaseData/dev.InterpreterBrokers.sql
+++ Tolk.BusinessLogic/BaseData/dev.InterpreterBrokers.sql
@@ -1,19 +1,18 @@
-
-Insert InterpreterBrokers (BrokerId, Email, FirstName, LastName, PhoneNumber)
-Values
-(1, 'tolk.ett@invalid', 'Tolk', 'Ett', '0700-0000001'),
-(1, 'tolk.tva@invalid', 'Tolk', 'Två', '0700-0000002'),
-(1, 'tolk.tre@invalid', 'Tolk', 'Tre', '0700-0000003'),
-(1, 'tolk.fyra@invalid', 'Tolk', 'Fyra', '0700-0000004'),
-(2, 'tolk.fem@invalid', 'Tolk', 'Fem', '0700-0000005'),
-(2, 'tolk.sex@invalid', 'Tolk', 'Sex', '0700-0000006'),
-(2, 'tolk.sju@invalid', 'Tolk', 'Sju', '0700-0000007'),
-(2, 'tolk.atta@invalid', 'Tolk', 'Åtta', '0700-0000008'),
-(3, 'tolk.nio@invalid', 'Tolk', 'Nio', '0700-0000009'),
-(3, 'tolk.tio@invalid', 'Tolk', 'Tio', '0700-0000010'),
-(3, 'tolk.elva@invalid', 'Tolk', 'Elva', '0700-0000011'),
-(3, 'tolk.tolv@invalid', 'Tolk', 'Tolv', '0700-0000012'),
-(4, 'tolk.tretton@invalid', 'Tolk', 'Tretton', '0700-0000013'),
-(4, 'tolk.fjorton@invalid', 'Tolk', 'Fjorton', '0700-0000014'),
-(4, 'tolk.femton@invalid', 'Tolk', 'Femton', '0700-0000015'),
-(4, 'tolk.sexton@invalid', 'Tolk', 'Sexton', '0700-0000016')
+INSERT InterpreterBrokers (BrokerId, Email, FirstName, LastName, PhoneNumber, IsActive)
+VALUES
+(1, 'tolk.ett@invalid', 'Tolk', 'Ett', '0700-0000001', 1),
+(1, 'tolk.tva@invalid', 'Tolk', 'Två', '0700-0000002', 1),
+(1, 'tolk.tre@invalid', 'Tolk', 'Tre', '0700-0000003', 1),
+(1, 'tolk.fyra@invalid', 'Tolk', 'Fyra', '0700-0000004', 1),
+(2, 'tolk.fem@invalid', 'Tolk', 'Fem', '0700-0000005', 1),
+(2, 'tolk.sex@invalid', 'Tolk', 'Sex', '0700-0000006', 1),
+(2, 'tolk.sju@invalid', 'Tolk', 'Sju', '0700-0000007', 1),
+(2, 'tolk.atta@invalid', 'Tolk', 'Åtta', '0700-0000008', 1),
+(3, 'tolk.nio@invalid', 'Tolk', 'Nio', '0700-0000009', 1),
+(3, 'tolk.tio@invalid', 'Tolk', 'Tio', '0700-0000010', 1),
+(3, 'tolk.elva@invalid', 'Tolk', 'Elva', '0700-0000011', 1),
+(3, 'tolk.tolv@invalid', 'Tolk', 'Tolv', '0700-0000012', 1),
+(4, 'tolk.tretton@invalid', 'Tolk', 'Tretton', '0700-0000013', 1),
+(4, 'tolk.fjorton@invalid', 'Tolk', 'Fjorton', '0700-0000014', 1),
+(4, 'tolk.femton@invalid', 'Tolk', 'Femton', '0700-0000015', 1),
+(4, 'tolk.sexton@invalid', 'Tolk', 'Sexton', '0700-0000016', 1)
