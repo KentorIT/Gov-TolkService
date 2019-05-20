7cebb187610e65ea3eb0dda95b048b77e2a083a2
johansvard <johan.svard@kentor.se>
2019-05-20 07:03:12 +0200

    Fixed bad spelling of role

--- Tolk.Web/Authorization/Roles.cs
+++ Tolk.Web/Authorization/Roles.cs
@@ -10,7 +10,7 @@ namespace Tolk.Web.Authorization
         public const string SystemAdministrator = nameof(SystemAdministrator);
         public const string Impersonator = nameof(Impersonator);
         public const string CentralAdministrator = nameof(CentralAdministrator);
-        public const string AppliationAdministrator = nameof(AppliationAdministrator);
+        public const string ApplicationAdministrator = nameof(ApplicationAdministrator);
         public const string CentralOrderHandler = nameof(CentralOrderHandler);
         public const string AdminRoles = nameof(SystemAdministrator) + ", " + nameof(CentralAdministrator);
     }
