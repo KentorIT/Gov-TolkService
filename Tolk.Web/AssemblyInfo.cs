ff4e3de74ef55895a7d00a532fd71d8515c7e9ba
johansvard <johan.svard@kentor.se>
2019-08-08 13:58:30 +0200

    Added assemblyInfo to public assemblies, to make it possible to set their version from build server...

--- Tolk.Web/AssemblyInfo.cs
+++ Tolk.Web/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Tolk.Web.Tests")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCompany("Sopra Steria AB, på uppdrag av Kammarkollegiet")]
