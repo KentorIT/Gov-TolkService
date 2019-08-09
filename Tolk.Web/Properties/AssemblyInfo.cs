c3e5c6612abe48b82d0f5447dba62242ec252af3
johansvard <johan.svard@kentor.se>
2019-08-09 10:25:43 +0200

    Had to remove the removal of automatic generation of AssemblyInfo, because this made the loading of secrets to stop(sic!)

--- Tolk.Web/Properties/AssemblyInfo.cs
+++ Tolk.Web/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Tolk.Web.Tests")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyCompany("Sopra Steria AB, på uppdrag av Kammarkollegiet")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyCompany("Sopra Steria AB, på uppdrag av Kammarkollegiet")]
