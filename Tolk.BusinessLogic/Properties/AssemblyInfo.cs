167fdfe91eb52b4ead92bfa3eb95dab602da1cb6
johansvard <johan.svard@kentor.se>
2019-08-13 07:39:00 +0200

    Changed back the AssemblyInfo settings, found another way of setting the version number, from dotnet build...

--- Tolk.BusinessLogic/Properties/AssemblyInfo.cs
+++ Tolk.BusinessLogic/Properties/AssemblyInfo.cs
@@ -1,8 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("Tolk.BusinessLogic.Tests")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyCompany("Sopra Steria AB, på uppdrag av Kammarkollegiet")]
-[assembly: AssemblyCopyright("Sopra Steria AB, på uppdrag av Kammarkollegiet")]
