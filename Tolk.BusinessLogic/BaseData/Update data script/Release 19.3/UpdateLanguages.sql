342ac0d3c67be0bdc7c73252ee568b5e4761c181
WINELL Liv <liv.winell@soprasteria.com>
2019-09-03 13:22:38 +0200

    #KT-1102 - Språk - lägg till BKS: Update data script - added BKS to the languages Bosniska, Kroatiska and Serbiska

--- Tolk.BusinessLogic/BaseData/Update data script/Release 19.3/UpdateLanguages.sql
+++ Tolk.BusinessLogic/BaseData/Update data script/Release 19.3/UpdateLanguages.sql
@@ -8,11 +8,24 @@ SELECT * FROM Orders o WHERE o.LanguageId IN (
 SELECT l.LanguageId FROM Languages l
 WHERE L.Name LIKE '%kines%' OR L.Name LIKE '%kanto%' OR L.Name LIKE '%mand%')
 
---uppdatera språk OBS! Också lägga till zho i Tellus:UnusedIsoCodes
+-- Innehåller ROLLBACK! Uppdatera språk OBS! Också lägga till zho i Tellus:UnusedIsoCodes
 BEGIN TRAN
 
 UPDATE Languages SET ACTIVE = 0 WHERE LanguageId = 59 AND Name = 'Kinesiska'
 
 UPDATE Languages SET NAME = 'Rikskinesiska (mandarin)' WHERE LanguageId = 96 AND Name = 'Rikskinesiska'
+
+ROLLBACK TRAN
+
+--uppdatera med BKS, Innehåller ROLLBACK!
+BEGIN TRAN
+
+SELECT * FROM Languages l WHERE L.TellusName='bos,hrv,srp'
+
+UPDATE Languages SET NAME = 'Bosniska (BKS)' WHERE TellusName='bos,hrv,srp' AND Name = 'Bosniska'
+UPDATE Languages SET NAME = 'Serbiska (BKS)' WHERE TellusName='bos,hrv,srp' AND Name = 'Serbiska'
+UPDATE Languages SET NAME = 'Kroatiska (BKS)' WHERE TellusName='bos,hrv,srp' AND Name = 'Kroatiska'
+
+SELECT * FROM Languages l WHERE L.TellusName='bos,hrv,srp'
 
 ROLLBACK TRAN
