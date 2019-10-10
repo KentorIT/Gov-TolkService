da74f4e537f431e8afadd9f1d3532e48888a331b
johansvard <johan.svard@kentor.se>
2019-10-10 16:59:23 +0200

    #KT-1121: Fixa User/List
    - Moved the user list to dynamic version.

--- Tolk.Web/Models/UserListItemModel.cs
+++ Tolk.Web/Models/UserListItemModel.cs
@@ -1,21 +1,28 @@
-﻿using Tolk.Web.Helpers;
+﻿using Tolk.Web.Attributes;
+using Tolk.Web.Helpers;
 
 namespace Tolk.Web.Models
 {
     public class UserListItemModel
     {
+        [ColumnDefinitions(IsIdColumn = true, Index = 0, Name = nameof(UserId), Visible = false)]
         public int UserId { get; set; }
 
+        [ColumnDefinitions(Index = 1, Name = nameof(Name), ColumnName = "NameFamily", SortOnWebServer = false, Title = "Namn")]
         public string Name { get; set; }
 
+        [ColumnDefinitions(Index = 2, Name = nameof(Email), Title = "Epost")]
         public string Email { get; set; }
 
+        [ColumnDefinitions(Index = 3, Name = nameof(Organisation), Title = "Organisation")]
         public string Organisation { get; set; }
 
+        [ColumnDefinitions(Index = 4, Name = nameof(LastLoginAt),SortOnWebServer = false, Title = "Senaste inloggning")]
         public string LastLoginAt { get; set; }
 
         public bool IsActive { get; set; }
 
+        [ColumnDefinitions(IsLeftCssClassName = true, Name = nameof(ColorClassName), Visible = false)]
         public string ColorClassName { get => CssClassHelper.GetColorClassNameForItemStatus(IsActive); }
     }
 }
