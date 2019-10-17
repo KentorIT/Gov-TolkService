38d6a6d48a4bbf7d88673561cb44fdfcf7c48c86
johansvard <johan.svard@kentor.se>
2019-10-17 09:21:30 +0200

    Added migration that was created due to a invers property in outboundWebHookCalls

---
+++ Tolk.BusinessLogic/Data/Migrations/20191017071806_WebHookAddFK.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tolk.BusinessLogic.Data.Migrations
+{
+    public partial class WebHookAddFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutboundWebHookCalls_ResentHookId",
+                table: "OutboundWebHookCalls");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboundWebHookCalls_ResentHookId",
+                table: "OutboundWebHookCalls",
+                column: "ResentHookId",
+                unique: true,
+                filter: "[ResentHookId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutboundWebHookCalls_ResentHookId",
+                table: "OutboundWebHookCalls");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboundWebHookCalls_ResentHookId",
+                table: "OutboundWebHookCalls",
+                column: "ResentHookId");
+        }
+    }
+}
