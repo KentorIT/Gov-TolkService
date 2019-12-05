51fb766e5182e1764c58326162351b218fce7126
WINELL Liv <liv.winell@soprasteria.com>
2019-12-05 19:36:46 +0100

    #KT-1222 - Det skall inte gå att lägga till samma tolk när man tillsätter två tolkar (extra tolk): Should not be possible to add same interpreter when assigning two interpreters (Part 1)

--- Tolk.Web/wwwroot/js/RequestGroup/Process.js
+++ Tolk.Web/wwwroot/js/RequestGroup/Process.js
@@ -1,17 +1,44 @@
-$(function () {
+﻿$(function () {
 
     var checkRequirements = function () {
-        $('#Accept').attr('disabled', false);
-
+        var requirementNotFulfilled = true;
         $("input[id$='CanMeetRequirement']:visible").each(function () {
             var isChecked = $(this).is(':checked');
             var isRequired = $('#' + $(this).attr('id').replace('CanMeetRequirement', 'IsRequired')).attr('value') === 'True';
 
             if (isRequired && !isChecked) {
                 $('#Accept').attr('disabled', true);
+                requirementNotFulfilled = false;
+                return false;
             }
         });
+        return requirementNotFulfilled;
     };
+
+    var checkSameInterpreter = function () {
+        //TODO ska få lägga till två skyddade
+        if ($("#InterpreterAnswerModel_InterpreterId").val() != "" && $("#InterpreterAnswerModel_InterpreterId").val() !== "-1" && $("#InterpreterAnswerModel_InterpreterId").val() === $("#ExtraInterpreterAnswerModel_InterpreterId").val()) {
+            triggerValidator("Det går inte att tillsätta samma tolk, ändra på någon av de tillsatta tolkarna", $("#interpreterIdValidator"))
+            return false;
+        }
+        else {
+            $("#interpreterIdValidator").hide();
+            return true;
+        }
+    };
+
+    function clientValidate() {
+        if (checkSameInterpreter() && checkRequirements()) {
+            $('#Accept').attr('disabled', false);
+        }
+    }
+
+    function triggerValidator(message, validatorId) {
+        $('#Accept').attr('disabled', true);
+        validatorId.empty();
+        validatorId.append(message);
+        validatorId.show();
+    }
 
     var setInterpreter = function (panel) {
         if ($(panel + " select[id$='InterpreterId'] option:selected").val() === "-1") {
@@ -31,15 +58,15 @@ $(function () {
         }
     };
     $('.interpreter-information-panel').on('shown.bs.collapse', function () {
-        checkRequirements();
+        clientValidate();
     }).on('hidden.bs.collapse', function () {
-        checkRequirements();
+        clientValidate();
     });
     var setExpectedTravelcost = function () {
         $(".expected-travel-costs-panel").collapse($("#InterpreterLocation option:selected").val() === "OffSitePhone" || $("#InterpreterLocation option:selected").val() === "OffSiteVideo" || $("#InterpreterLocation option:selected").val() === "" ?
             'hide' : 'show');
     };
-        
+
     checkRequirements();
     setExpectedTravelcost();
     setInterpreter();
@@ -53,10 +80,12 @@ $(function () {
     });
 
     $('#InterpreterAnswerModel_InterpreterId').change(function () {
+        clientValidate();
         setInterpreter(".interpreter-selection-panel");
     });
 
     $('#ExtraInterpreterAnswerModel_InterpreterId').change(function () {
+        clientValidate();
         setInterpreter(".extra-interpreter-selection-panel");
         handlePartialDecline(".outer-extra-interpreter-panel");
     });
@@ -68,7 +97,7 @@ $(function () {
     });
 
     $('.checkbox').change(function () {
-        checkRequirements();
+        clientValidate();
     });
 
     //handle cancel/back to previous page
