7adb2abf2828bbeb4309d63aa045010788bfdc48
johansvard <johan.svard@kentor.se>
2020-01-10 15:16:35 +0100

    #KT-1276: Gör det möjligt att rensa de cacher som systemet har.
    - Added a flush caches to api as well.

--- Tolk.Web.Api/Controllers/HomeController.cs
+++ Tolk.Web.Api/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using NSwag.Annotations;
 using System.ComponentModel;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Tolk.BusinessLogic.Services;
 using Tolk.BusinessLogic.Utilities;
 
@@ -14,10 +15,12 @@ namespace Tolk.Web.Api.Controllers
     public class HomeController : ControllerBase
     {
         private readonly ISwedishClock _timeService;
+        private readonly CacheService _cacheService;
 
-        public HomeController(ISwedishClock timeService)
+        public HomeController(ISwedishClock timeService, CacheService cacheService)
         {
             _timeService = timeService;
+            _cacheService = cacheService;
         }
 
         [HttpGet]
@@ -42,6 +45,16 @@ namespace Tolk.Web.Api.Controllers
             X509Certificate2 clientCertInRequest = Request.HttpContext.Connection.ClientCertificate;
             return clientCertInRequest?.SerialNumber;
         }
+
+        [HttpGet]
+        [OpenApiIgnore]
+        public async Task<ActionResult<string>> FlushCaches()
+        {
+            await _cacheService.FlushAll();
+            //Since it only is three caches right now, this does not have to be authentication-hidden.
+            return "Cacherna har rensats.";
+        }
+
 
         [HttpGet]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "MVC method, cannot be static")]
