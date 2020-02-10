dd44e3b787e9406ddb79e531e52431308f2bfd13
WINELL Liv <liv.winell@soprasteria.com>
2020-02-10 11:55:01 +0100

    Fix with scripts to next release

--- Tolk.BusinessLogic/BaseData/Update data script/Release 1.5/InsertNewNotificationTypes.sql
+++ Tolk.BusinessLogic/BaseData/Update data script/Release 1.5/InsertNewNotificationTypes.sql
@@ -7,10 +7,12 @@ Select * from AspnetUsers
 Where IsApiUser = 1 
 
 
--- For each of them that does not have an email setting (NotificationChannel = 1) on NotificationType > 22
+-- For each of them that does not have an email setting (NotificationChannel = 1) on NotificationType > 22 NOT FOR TYPE 25!
 -- Look in web code, to make sure that all fields in the enum NotificationType is represented for all users.
 -- Could remove all specified emails in ConnectionInformation, as long as they are the same as the api-user's since that amounts to the same.
 
+
+---OBS!lägg INTE till nummer 25 eftersom vi inte sagt något om att det kommer och de aldrig förut fått notifiering om när uppdragstid är förbi...
 
 
 INSERT UserNotificationSettings (UserId, NotificationChannel, NotificationType)
--- Tolk.BusinessLogic/BaseData/Update data script/Release 1.5/InsertNoAnswerConfirmations.sql
+++ Tolk.BusinessLogic/BaseData/Update data script/Release 1.5/InsertNoAnswerConfirmations.sql
@@ -1,5 +1,6 @@
 
 
+--order i Dev som vi lagt in bekräftelser för 9, 15, 38, 53, 63, 64, 83, 97, 159, 319, 320
 
 --om vi inte vill göra ConfirmedBy nullable (inte gjort vid denna incheckning)
 --så kan man sätta att den som tillsatt också bekräftat att det inte blev något (finns ingen än så länge i prod. 24 jan)
@@ -21,10 +22,10 @@ ROLLBACK TRAN
 
 --annars måste göra den nullable för att "systemet" ska kunna bekräfta om ej ta API-user
 
-SELECT * FROM Requests r WHERE r.Status = 16
+--SELECT * FROM Requests r WHERE r.Status = 16
 
-INSERT INTO RequestStatusConfirmation (RequestId, RequestStatus, ConfirmedAt, ConfirmedBy, ImpersonatingConfirmedBy)
-	SELECT r.RequestId, 16, o.StartAt, NULL, NULL
-	FROM Requests r
-	JOIN Orders o ON r.OrderId = o.OrderId
-	 WHERE r.Status = 16
+--INSERT INTO RequestStatusConfirmation (RequestId, RequestStatus, ConfirmedAt, ConfirmedBy, ImpersonatingConfirmedBy)
+--	SELECT r.RequestId, 16, o.StartAt, NULL, NULL
+--	FROM Requests r
+--	JOIN Orders o ON r.OrderId = o.OrderId
+--	 WHERE r.Status = 16
