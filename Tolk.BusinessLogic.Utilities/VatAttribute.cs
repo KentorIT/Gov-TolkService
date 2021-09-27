19324f2e4877fb858a8578f4947446fbe5220d1b
Johan Svärd <johan.svard@soprasteria.com>
2021-09-27 10:44:36 +0200

    KT-1421: Skapa artikellistan som skall användas för orderöverenskommelsen
    - Added enum for articles, and attribute for vat to be used ni the aforementioned enum

---
+++ Tolk.BusinessLogic.Utilities/VatAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tolk.BusinessLogic.Utilities
+{
+    /// <summary>
+    /// Used to set vat on invoiceable articles.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class VatAttribute : Attribute
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public VatAttribute(double vat)
+        {
+            Vat = vat;
+        }
+
+        /// <summary>
+        /// The parent
+        /// </summary>
+        public double Vat { get; private set; }
+    }
+}
---
+++ Tolk.BusinessLogic/Enums/InvoiceableArticle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Tolk.BusinessLogic.Utilities;
+
+namespace Tolk.BusinessLogic.Enums
+{
+    public enum InvoiceableArticle
+    {
+        [Vat(0.25)]
+        [CustomName("interpreter_compensation_incl_social_insurance")]
+        [Description("Summa tolkersättning inkl.sociala avgifter")]
+        InterpreterCompensationIncludingSocialCharge = 1,
+
+        [Vat(0.25)]
+        [CustomName("administrative_charge")]
+        [Description("Administrativ avgift")]
+        AdministrativeCharge = 2,
+
+        [Vat(0.25)]
+        [CustomName("broker_fee")]
+        [Description("Förmedlingsavgift")]
+        BrokerFee = 3,
+
+        [Vat(0.25)]
+        [CustomName("travel_cost")]
+        [Description("Resekostnad")]
+        TravelCost = 4
+    }
+}
