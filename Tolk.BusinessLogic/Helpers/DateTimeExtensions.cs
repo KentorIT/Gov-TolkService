88d7529e89a365f1f607cd4d09c45c22b14fdae2
Johan Svärd <johan.svard@soprasteria.com>
2022-09-13 13:35:48 +0200

    Fix for failing tests on git actions, that run on linux servers.
    Had to handle differing naming-conventions for time zones on windows and non-windows environments.

--- Tolk.BusinessLogic/Helpers/DateTimeExtensions.cs
+++ Tolk.BusinessLogic/Helpers/DateTimeExtensions.cs
@@ -5,8 +5,11 @@ namespace Tolk.BusinessLogic.Helpers
 {
     public static class DateTimeExtensions
     {
+        //Try getting timezone by windows name standard first, and if that fails, try the linux way.
+        // used if running tests on non-windows machines
         private static readonly TimeZoneInfo timeZoneInfo =
-            TimeZoneInfo.GetSystemTimeZones().Single(tzi => tzi.Id == "W. Europe Standard Time");
+            TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time") ??
+            TimeZoneInfo.FindSystemTimeZoneById("Europe/Stockholm");
 
         /// <summary>
         /// Convert a raw user-inserted date time into a DateTimeOffset, observering
