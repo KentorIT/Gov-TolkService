558f99d1d268d19a3a13604bbba42385a7c53b4f
Adam Kolterud <adam.kolterud@soprasteria.com>
2022-10-21 15:43:47 +0200

    Added ContractDefinition to FrameworkAgreementResponseRuleset, Created displayclass for Contract

--- Tolk.BusinessLogic/Enums/FrameworkAgreementResponseRuleset.cs
+++ Tolk.BusinessLogic/Enums/FrameworkAgreementResponseRuleset.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel;
-
+using Tolk.BusinessLogic.Utilities;
 
 namespace Tolk.BusinessLogic.Enums
 {
     public enum FrameworkAgreementResponseRuleset
     {
         [Description("Första versionen avtalet")]
+        [ContractDefinition(RankingRules = "För detta ramavtalsområde gäller en fastställd rangordning per län. En fast rangordning innebär att avropsförfrågan ska ställas till den först rangordnade leverantören (tolkförmedlingen) inom det aktuella länet.")]
         VersionOne = 1,
         [Description("Andra versionen av avtalet, med andra svarsregler")]
+        [ContractDefinition(RankingRules = "För detta ramavtalsområde gäller Något helt annat")]
         VersionTwo = 2
     }
 }
