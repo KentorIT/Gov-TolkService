edc0ba7a1f7ba279a0efee56f1486c742235bc2e
Adam Kolterud <adam.kolterud@soprasteria.com>
2022-10-21 15:42:35 +0200

    Created ContractDefinitionAttribute with connected "Display Class"

---
+++ Tolk.BusinessLogic.Utilities/ContractDefinition.cs
@@ -0,0 +1,15 @@
+﻿namespace Tolk.BusinessLogic.Utilities
+{
+    public class ContractDefinition
+    {
+
+        public string Usage { get; set; }
+        public string Includes { get; set; }
+        public string ExcludedServices { get; set; }
+        public string RankingRules { get; set; }
+        public string GeneralTerms { get; set; }
+
+    }
+
+}
+
