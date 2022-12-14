329dccd09b884a08ce2089d046d1bb4eb7c9ec38
Johan Svärd <johan.svard@soprasteria.com>
2022-12-14 08:54:54 +0100

    KT-1527: Fel färg på förfrågningar och betställningar som har utgått på grund av utgånget avtal
    - Added TerminatedDueToTerminatedFrameworkAgreement as a "red" status for request and order

--- Tolk.Web/Helpers/CssClassHelper.cs
+++ Tolk.Web/Helpers/CssClassHelper.cs
@@ -6,7 +6,7 @@ namespace Tolk.Web.Helpers
     {
         public static string GetColorClassNameForOrderStatus(OrderStatus status)
         {
-            return (status == OrderStatus.NoBrokerAcceptedOrder || status == OrderStatus.CancelledByCreator || status == OrderStatus.CancelledByBroker || status == OrderStatus.ResponseNotAnsweredByCreator || status == OrderStatus.NoDeadlineFromCustomer) ? "red-border-left"
+            return (status == OrderStatus.NoBrokerAcceptedOrder || status == OrderStatus.CancelledByCreator || status == OrderStatus.CancelledByBroker || status == OrderStatus.ResponseNotAnsweredByCreator || status == OrderStatus.NoDeadlineFromCustomer || status == OrderStatus.TerminatedDueToTerminatedFrameworkAgreement) ? "red-border-left"
                 : (status == OrderStatus.Delivered || status == OrderStatus.ResponseAccepted) ? "green-border-left"
                 : (status == OrderStatus.RequestRespondedAwaitingApproval || status == OrderStatus.RequestRespondedNewInterpreter || status == OrderStatus.AwaitingDeadlineFromCustomer) ? "yellow-border-left"
                 : "blue-border-left";
@@ -19,7 +19,7 @@ namespace Tolk.Web.Helpers
 
         public static string GetColorClassNameForRequestStatus(RequestStatus status)
         {
-            return (status == RequestStatus.CancelledByBroker || status == RequestStatus.CancelledByCreator || status == RequestStatus.CancelledByCreatorWhenApproved || status == RequestStatus.DeniedByCreator || status == RequestStatus.DeniedByTimeLimit || status == RequestStatus.ResponseNotAnsweredByCreator || status == RequestStatus.DeclinedByBroker || status == RequestStatus.NoDeadlineFromCustomer) ? "red-border-left"
+            return (status == RequestStatus.CancelledByBroker || status == RequestStatus.CancelledByCreator || status == RequestStatus.CancelledByCreatorWhenApproved || status == RequestStatus.DeniedByCreator || status == RequestStatus.DeniedByTimeLimit || status == RequestStatus.ResponseNotAnsweredByCreator || status == RequestStatus.DeclinedByBroker || status == RequestStatus.NoDeadlineFromCustomer || status == RequestStatus.TerminatedDueToTerminatedFrameworkAgreement) ? "red-border-left"
                 : (status == RequestStatus.Approved || status == RequestStatus.Delivered) ? "green-border-left"
                 : (status == RequestStatus.AnsweredAwaitingApproval || status == RequestStatus.AcceptedNewInterpreterAppointed || status == RequestStatus.AwaitingDeadlineFromCustomer) ? "yellow-border-left"
                 : "blue-border-left";
