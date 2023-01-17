527e5d5a6066e9b5b7481dc7e5b8c236bb69d3c5
Adam Kolterud <adam.kolterud@soprasteria.com>
2023-01-17 09:25:45 +0100

    KT-1559 - ReadOnly Display-property for BrokerFeePercentage Added and used in view

--- Tolk.Web/Models/BrokerRankModel.cs
+++ Tolk.Web/Models/BrokerRankModel.cs
@@ -9,6 +9,7 @@ namespace Tolk.Web.Models
         public int Rank { get; set; }
 
         public decimal BrokerFeePercentage { get; set; }
+        public string BrokerFeePercentageDisplay => BrokerFeePercentage.ToString("P");
 
         public IEnumerable<string> BrokerFeesPerCompetenceLevel { get; set; }
 
