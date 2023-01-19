f845dda06c9c1b1fd45b6d8dd8791a13e71959a2
Johan Svärd <johan.svard@soprasteria.com>
2023-01-19 07:18:26 +0100

    KT-1562: Fixa kodgranskningar från flera tickets
    - KT-1480: Removed two unused lists, and added inheritance between two models for leaner code.

--- Tolk.Web/Models/RequestGroupAnswerModel.cs
+++ Tolk.Web/Models/RequestGroupAnswerModel.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using Tolk.BusinessLogic.Enums;
 
 namespace Tolk.Web.Models
 {
-    public class RequestGroupAnswerModel : IModel
+    public class RequestGroupAnswerModel : RequestGroupAcceptModel, IModel
     {
-        public int RequestGroupId { get; set; }
-
         public InterpreterLocation? InterpreterLocation { get; set; }
 
         public RadioButtonGroup SetLatestAnswerTimeForCustomer { get; set; }
@@ -17,13 +14,5 @@ namespace Tolk.Web.Models
         public InterpreterAnswerModel InterpreterAnswerModel { get; set; }
 
         public InterpreterAnswerModel ExtraInterpreterAnswerModel { get; set; }
-
-        public List<RequestRequirementAnswerModel> RequiredRequirementAnswers { get; set; } = new List<RequestRequirementAnswerModel>();
-
-        public List<RequestRequirementAnswerModel> DesiredRequirementAnswers { get; set; } = new List<RequestRequirementAnswerModel>();
-
-        public string BrokerReferenceNumber { get; set; }
-
-        public List<FileModel> Files { get; set; }
     }
 }
