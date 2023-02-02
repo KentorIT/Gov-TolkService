0d9281fc8e02eede11e4709fb3a91b473474902d
Johan Svärd <johan.svard@soprasteria.com>
2023-02-02 09:07:03 +0100

    KT-1491: Flexibel tidsbokning: Skapa möjlighet att sätta en tidsperiod (initialt arbetsdagar mellan 8-17) och förväntad längd på tillfället
    - Fixed bug where replacement timespans were not bound correctly.

--- Tolk.Web/Services/TimeSpanModelBinder.cs
+++ Tolk.Web/Services/TimeSpanModelBinder.cs
@@ -12,7 +12,18 @@ namespace Tolk.Web.Services
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
             NullCheckHelper.ArgumentCheckNull(bindingContext, nameof(TimeSpanModelBinder));
+            if (bindingContext.ValueProvider.GetValue($"{bindingContext.ModelName}") != ValueProviderResult.None)
+            {
+                return BindSingleInput(bindingContext);
+            }
+            else
+            {
+                return BindSeparated(bindingContext);
+            }
+        }
 
+        private static Task BindSeparated(ModelBindingContext bindingContext)
+        {
             var timeHourValue = bindingContext.ValueProvider.GetValue($"{bindingContext.ModelName}.Hours");
             var timeMinuteValue = bindingContext.ValueProvider.GetValue($"{bindingContext.ModelName}.Minutes");
 
@@ -22,6 +33,36 @@ namespace Tolk.Web.Services
             }
 
             var model = new TimeSpan(timeHourValue.FirstValue.ToSwedishInt(), timeMinuteValue.FirstValue.ToSwedishInt(), 0);
+
+            bindingContext.Result = ModelBindingResult.Success(model);
+
+            return Task.CompletedTask;
+        }
+
+        private static Task BindSingleInput(ModelBindingContext bindingContext)
+        {
+            var timeValue = bindingContext.ValueProvider.GetValue($"{bindingContext.ModelName}");
+
+            if (timeValue == ValueProviderResult.None
+                || string.IsNullOrWhiteSpace(timeValue.FirstValue))
+            {
+                return Task.CompletedTask;
+            }
+            string timeValueSanitized;
+
+            if (timeValue.FirstValue == "0"
+                || timeValue.FirstValue == "00")
+            {
+                timeValueSanitized = "00:00";
+            }
+            else
+            {
+                timeValueSanitized = timeValue.FirstValue.ContainsSwedish(":")
+                    ? timeValue.FirstValue
+                    : timeValue.FirstValue.Insert(timeValue.FirstValue.Length - 2, ":"); // Add colon to time if not exists
+            }
+
+            var model = timeValueSanitized.ToSwedishTimeSpan();
 
             bindingContext.Result = ModelBindingResult.Success(model);
 
