2052bdae0570763e091b7b67b07ab640409a0c93
livwin <liv.winell@soprasteria.com>
2023-04-17 14:18:33 +0200

    KT-1601 - Sql-skript - sätt korrekt startdatum på BrokerFeeByServiceTypePriceListRows
    Script to update BrokerFeeByServiceTypePriceListRows with correct start date

---
+++ Tolk.BusinessLogic/BaseData/Update data script/Release 2.1.1/BrokerFeeByServiceTypePriceListRows_SetCorrectFirstValidDate.sql
@@ -0,0 +1,6 @@
+
+--48 rader, kolla datum (bör vara 2023-02-14), sätt till '2023-02-15'
+select * from BrokerFeeByServiceTypePriceListRows
+
+Update BrokerFeeByServiceTypePriceListRows set FirstValidDate = '2023-02-15'
+
