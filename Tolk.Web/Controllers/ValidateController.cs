4e6274668744b40b8d8ca60cb4d35b00156fe8a1
AdamKolterud <adam.kolterud@soprasteria.com>
2023-06-16 15:24:29 +0200

    KT-1656 - Added logging if Customer is missing Settings for CustomerSpecific Properties - user type of property to validate instead of hardcoded InvoiceReference - made validationmethod private

--- Tolk.Web/Controllers/ValidateController.cs
+++ Tolk.Web/Controllers/ValidateController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Tolk.BusinessLogic.Enums;
@@ -16,18 +17,21 @@ namespace Tolk.Web.Controllers
     public class ValidateController : Controller
     {                           
         private readonly CacheService _cacheService;
+        private readonly ILogger _logger;
         private const string CustomerSpecificInvoice= $"{nameof(CustomerSpecificInvoiceReference)}.Value";
-        public ValidateController(CacheService cacheService)
+        public ValidateController(CacheService cacheService, ILogger<ValidateController> logger)
         {                                           
             _cacheService = cacheService;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult CustomerSpecificInvoiceReference([Bind(Prefix = CustomerSpecificInvoice)] string value)
         {
             var customerOrganisationId = User.GetCustomerOrganisationId();
-            var invoiceProperty = _cacheService.CustomerSpecificProperties.Where(csp => csp.CustomerOrganisationId == customerOrganisationId && csp.PropertyToReplace == PropertyType.InvoiceReference).Single();
+            var invoiceProperty = _cacheService.CustomerSpecificProperties.Where(csp => csp.CustomerOrganisationId == customerOrganisationId && csp.PropertyToReplace == PropertyType.InvoiceReference).Single();        
             var validationResult = ValidateCustomerSpecificProperty(invoiceProperty, value);
+
             if (!validationResult.Success)
             {                
                 return Json(validationResult.ErrorMessage);
@@ -35,11 +39,12 @@ namespace Tolk.Web.Controllers
             return Json(validationResult.Success);
         }
 
-        public (bool Success, string ErrorMessage ) ValidateCustomerSpecificProperty(CustomerSpecificPropertyModel property, string value)
+        private (bool Success, string ErrorMessage ) ValidateCustomerSpecificProperty(CustomerSpecificPropertyModel property, string value)
         {
             if (property == null)
             {
-                return new (false , $"Myndighet med id {property.CustomerOrganisationId} har ingen kundspecifik inställning för {PropertyType.InvoiceReference.GetDescription()}");
+                _logger.LogInformation("Myndighet med id {customerOrganisationId} har ingen kundspecifik inställning för {property}", property.CustomerOrganisationId, property.PropertyToReplace.GetDescription());
+                return (false, $"Kundspecifik inställning för {property.PropertyToReplace.GetDescription()} saknas");
             }
             var regexChecker = new Regex(property.RegexPattern);
             if(!regexChecker.Match(value).Success)
