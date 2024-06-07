27bdadd498cae9ce9fc4a6a5e4b2d98fc1afc1b7
AdamKolterud <adam.kolterud@soprasteria.com>
2024-06-07 08:12:15 +0200

    Revert "KT-1672 Changed DateTime to DateTime Offset"
    This reverts commit f96de1fb724873f4c18a43c4a134c8ffa55215e5.

--- Tolk.BusinessLogic/Services/CustomerOrganisationService.cs
+++ Tolk.BusinessLogic/Services/CustomerOrganisationService.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Tolk.BusinessLogic.Data;
 using Tolk.BusinessLogic.Entities;
-using Tolk.BusinessLogic.Helpers;
 using Tolk.BusinessLogic.Utilities;
 
 namespace Tolk.BusinessLogic.Services
@@ -72,7 +71,7 @@ namespace Tolk.BusinessLogic.Services
             var customer = await _dbContext.CustomerOrganisations.Include(c => c.CustomerOrderAgreementSettings).GetCustomerById(customerOrganisationId);            
             customer.UpdateCustomerOrderAgreementBrokerSettings(_clock.SwedenNow, userId);
             var setting = customer.CustomerOrderAgreementSettings.Where(coas => coas.CustomerOrganisationId == customerOrganisationId && coas.BrokerId == brokerId).SingleOrDefault();
-            var enabledAt = _clock.SwedenNow < customer.UseOrderAgreementsFromDate.Value.ToDateTimeOffsetSweden() ? customer.UseOrderAgreementsFromDate.Value.ToDateTimeOffsetSweden() : _clock.SwedenNow;
+            var enabledAt = _clock.SwedenNow.DateTime < customer.UseOrderAgreementsFromDate ? customer.UseOrderAgreementsFromDate : _clock.SwedenNow.DateTime;
             setting.EnabledAt = setting.Disabled ? enabledAt : null;
 
             await _dbContext.SaveChangesAsync();
