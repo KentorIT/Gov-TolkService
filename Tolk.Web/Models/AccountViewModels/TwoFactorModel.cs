862d64b95110c985bf8e54f530bad2a2c7ffa97a
Johan Svärd <johan.svard@soprasteria.com>
2026-01-27 10:14:17 +0100

    KT-1691: Implementera tvåfaktorsinloggning via epost
    - Fixed texts as per requested

--- Tolk.Web/Models/AccountViewModels/TwoFactorModel.cs
+++ Tolk.Web/Models/AccountViewModels/TwoFactorModel.cs
@@ -8,7 +8,7 @@ namespace Tolk.Web.Models.AccountViewModels
     {
         [Required(ErrorMessage = "Valideringskod måste anges")]
         [NoAutoComplete]
-        [Display(Name = "Valideringskod", Description = "Koden skickas till din registrerade e‑postadress.\nOm du inte ser den inom ett par minuter, kontrollera gärna din skräppostmapp. Om koden fortfarande inte har kommit inom fem minuter, testa att skicka efter en ny kod.\nOm det fortfarande inte fungerar, kontakta vår support för hjälp. Se kontaktuppgifter på botten av sidan.")]
+        [Display(Name = "Valideringskod", Description = "Koden skickas till din registrerade e‑postadress. Om du inte ser den inom ett par minuter, kontrollera gärna din skräppostmapp. Om koden fortfarande inte har kommit inom fem minuter, testa att skicka efter en ny kod. Om det fortfarande inte fungerar, kontakta vår support för hjälp. Se kontaktuppgifter i sidfoten.")]
         public string Code { get; set; }
         public Uri ReturnUrl { get; set; }
         public string Message { get; set; }
